ced9c63c12149bc2b42a6789186e4dd41dc96d9e
Donald Piret <donald@donaldpiret.com>
2013-09-03 18:23:15 +0800

    Add boxen to the path

--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,2 @@
-export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:$HOME/bin:/usr/local/share/python:$DOTFILES/bin
+export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/boxen/bin:$HOME/bin:/usr/local/share/python:$DOTFILES/bin
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
